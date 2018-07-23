3b67da6871aff9eab919ca4a198a59b900b50916
Viricel Marc <marc.viricel@nokia.com>
2018-07-23 23:52:57 +0200

    initial game of a ship landing on a platform

--- ProjectOmega/GameScene.swift
+++ ProjectOmega/GameScene.swift
@@ -10,80 +10,66 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+
+    private var spaceship: SKSpriteNode?
+
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        addBackground()
+        addPlanetGround()
+        addSpaceship()
+        addLandingPlatform()
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+
+    private func addBackground() {
+        let spaceBackground = SKSpriteNode(imageNamed: "BlueSpace.jpg")
+        spaceBackground.position = CGPoint(x: size.width/2, y: size.height/2)
+        spaceBackground.zPosition = -2
+        addChild(spaceBackground)
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+
+    private func addPlanetGround() {
+        let planetGround = SKSpriteNode(imageNamed: "MoonSurface")
+        planetGround.scale(to: CGSize(width: size.width, height: size.height/4))
+        planetGround.position = CGPoint(x: planetGround.size.width/2, y: 0)
+        planetGround.zPosition = -1
+        let planetGroundPhysic = SKPhysicsBody(rectangleOf: planetGround.frame.size)
+        planetGroundPhysic.friction = 0.8
+        planetGroundPhysic.isDynamic = false
+        planetGround.physicsBody = planetGroundPhysic
+        addChild(planetGround)
     }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+
+    private func addSpaceship() {
+        let spaceship = SKSpriteNode(imageNamed: "Spaceship")
+        spaceship.size = CGSize(width: 100, height: 100)
+        spaceship.position = CGPoint(x: 100, y: size.height)
+        spaceship.zPosition = 0
+        let bodySize = CGSize(width: 0.8 * spaceship.size.width, height: 0.8 * spaceship.size.height)
+        let spaceshipPhysics = SKPhysicsBody(rectangleOf: bodySize)
+        spaceshipPhysics.friction = 0.8
+        spaceship.physicsBody = spaceshipPhysics
+        self.spaceship = spaceship
+        addChild(spaceship)
     }
-    
+
+    private func addLandingPlatform() {
+        let landingNode = SKShapeNode(rectOf: CGSize(width: 200, height: 40), cornerRadius: 10)
+        landingNode.fillColor = .gray
+        landingNode.position = CGPoint(x: 0.8 * size.width, y: 0.5 * size.height)
+        let landingNodeBody = SKPhysicsBody(rectangleOf: landingNode.frame.size)
+        landingNodeBody.isDynamic = false
+        landingNode.physicsBody = landingNodeBody
+        addChild(landingNode)
+    }
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        guard let mainTouchLocation = touches.first?.location(in: self),
+        let spaceship = self.spaceship else {
+            return
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        var forceVector = CGVector(dx: mainTouchLocation.x - spaceship.position.x, dy: mainTouchLocation.y - spaceship.position.y)
+        forceVector.dx = forceVector.dx < 0 ? -40 : 40
+        forceVector.dy = forceVector.dy < 0 ? -120 : 120
+        spaceship.physicsBody?.applyImpulse(forceVector)
     }
 }
--- ProjectOmega/GameViewController.swift
+++ ProjectOmega/GameViewController.swift
@@ -14,42 +14,18 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
+
+            let scene = GameScene(size: view.bounds.size)
+            scene.scaleMode = .aspectFill
+
+            view.presentScene(scene)
+
             view.ignoresSiblingOrder = true
-            
             view.showsFPS = true
             view.showsNodeCount = true
+            view.showsPhysics = true
         }
-    }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
     }
 }
